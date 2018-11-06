3c65e201a4e880599177539d099a3412149adfc7
kconcon3 <hkane3sub@excite.co.jp>
2018-11-06 19:42:55 +0900

    first commit

---
+++ Assets/Scripts/AnimEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEnd : StateMachineBehaviour {
+
+	// Mecanimのアニメーションコントローラーの再生の修了用
+	// 新しいステートに移り変わった時に実行される関数なので
+	// 最後に空のステートを作ってAdd Behaviourでこのスクリプトを追加する
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+		Destroy(animator.gameObject);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # UnityMecanimPlayOnceTest01
+
+## 概要  
+UnityのMecanim(Animator)を使って再生したアニメーションが終わったらGameObjectごと消す
+<br /> 
+## テストした時の環境  
+Windows7 Pro 64bit  
+Unity 2018.2.10f1  
+Visual Studio Community 2017  
+
+
+
+
